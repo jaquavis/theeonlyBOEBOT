1d163fb2251baf813822bcecfffb54438c8b96bf
OrbitUT <nuggyut@gmail.com>
2019-03-15 19:55:31 -0600

    Update JustinMN.h

--- JustinMN.h
+++ JustinMN.h
@@ -1,4 +1,4 @@
-* Note lengths */
+/* Note lengths */
 #define MEASURE_TIME              2000  /* Time in ms for 1 measure (1 full note) - should be divisible by 16 */
 #define FULL_NOTE                 (MEASURE_TIME)
 #define HALF_NOTE                 (MEASURE_TIME / 2)
@@ -6,11 +6,11 @@
 #define EIGHTH_NOTE               (MEASURE_TIME / 8)
 #define SIXTEENTH_NOTE            (MEASURE_TIME / 16)
 
-#define FN                        FULL_NOTE                 
-#define HN                        HALF_NOTE                 
-#define QN                        QUARTER_NOTE              
-#define EN                        EIGHTH_NOTE               
-#define SN                        SIXTEENTH_NOTE            
+#define FNj                        FULL_NOTE
+#define HNj                        HALF_NOTE
+#define QNj                        QUARTER_NOTE
+#define ENj                        EIGHTH_NOTE
+#define SNj                        SIXTEENTH_NOTE
 
 /* Note length adjustments */
 #define REGULAR_NOTE_ADJUSTMENT   50
@@ -18,8 +18,8 @@
 #define HOLD_NOTE_ADJUSTMENT      0
 
 #define RT                        REGULAR_NOTE_ADJUSTMENT
-#define ST                        STACCATO_NOTE_TIME        
-#define HT                        HOLD_NOTE_ADJUSTMENT            
+#define ST                        STACCATO_NOTE_TIME
+#define HT                        HOLD_NOTE_ADJUSTMENT
 
 /* Musical note definitions */
 #define NOTE_C3                   131
@@ -76,52 +76,52 @@
 #define NONE                      0
 
 /* Musical note definitions - short hand */
-#define C3                   NOTE_C3
-#define C3S                  NOTE_C3_SHARP
-#define D3                   NOTE_D3
-#define D3S                  NOTE_D3_SHARP
-#define E3                   NOTE_E3
-#define F3                   NOTE_F3
-#define F3S                  NOTE_F3_SHARP
-#define G3                   NOTE_G3
-#define G3S                  NOTE_G3_SHARP
-#define A3                   NOTE_A3
-#define A3S                  NOTE_A3_SHARP
-#define B3                   NOTE_B3
-#define C4                   NOTE_C4  /* Middle C */
-#define C4S                  NOTE_C4_SHARP
-#define D4                   NOTE_D4
-#define D4S                  NOTE_D4_SHARP
-#define E4                   NOTE_E4
-#define F4                   NOTE_F4
-#define F4S                  NOTE_F4_SHARP
-#define G4                   NOTE_G4
-#define G4S                  NOTE_G4_SHARP
-#define A4                   NOTE_A4
-#define A4S                  NOTE_A4_SHARP
-#define B4                   NOTE_B4
-#define C5                   NOTE_C5
-#define C5S                  NOTE_C5_SHARP
-#define D5                   NOTE_D5
-#define D5S                  NOTE_D5_SHARP
-#define E5                   NOTE_E5
-#define F5                   NOTE_F5
-#define F5S                  NOTE_F5_SHARP
-#define G5                   NOTE_G5
-#define G5S                  NOTE_G5_SHARP
-#define A5                   NOTE_A5
-#define A5S                  NOTE_A5_SHARP
-#define B5                   NOTE_B5
-#define C6                   NOTE_C6
-#define C6S                  NOTE_C6_SHARP
-#define D6                   NOTE_D6
-#define D6S                  NOTE_D6_SHARP
-#define E6                   NOTE_E6
-#define F6                   NOTE_F6
-#define F6S                  NOTE_F6_SHARP
-#define G6                   NOTE_G6
-#define G6S                  NOTE_G6_SHARP
-#define A6                   NOTE_A6
-#define A6S                  NOTE_A6_SHARP
-#define B6                   NOTE_B6
-#define NO                   NONE
+#define C3j                   NOTE_C3
+#define C3Sj                  NOTE_C3_SHARP
+#define D3j                   NOTE_D3
+#define D3Sj                  NOTE_D3_SHARP
+#define E3j                   NOTE_E3
+#define F3j                   NOTE_F3
+#define F3Sj                  NOTE_F3_SHARP
+#define G3j                   NOTE_G3
+#define G3Sj                  NOTE_G3_SHARP
+#define A3j                   NOTE_A3
+#define A3Sj                  NOTE_A3_SHARP
+#define B3j                   NOTE_B3
+#define C4j                   NOTE_C4  /* Middle C */
+#define C4Sj                  NOTE_C4_SHARP
+#define D4j                   NOTE_D4
+#define D4Sj                  NOTE_D4_SHARP
+#define E4j                   NOTE_E4
+#define F4j                   NOTE_F4
+#define F4Sj                  NOTE_F4_SHARP
+#define G4j                   NOTE_G4
+#define G4Sj                  NOTE_G4_SHARP
+#define A4j                   NOTE_A4
+#define A4Sj                  NOTE_A4_SHARP
+#define B4j                   NOTE_B4
+#define C5j                   NOTE_C5
+#define C5Sj                  NOTE_C5_SHARP
+#define D5j                   NOTE_D5
+#define D5Sj                  NOTE_D5_SHARP
+#define E5j                   NOTE_E5
+#define F5j                   NOTE_F5
+#define F5Sj                  NOTE_F5_SHARP
+#define G5j                   NOTE_G5
+#define G5Sj                  NOTE_G5_SHARP
+#define A5j                   NOTE_A5
+#define A5Sj                  NOTE_A5_SHARP
+#define B5j                   NOTE_B5
+#define C6j                   NOTE_C6
+#define C6Sj                  NOTE_C6_SHARP
+#define D6j                   NOTE_D6
+#define D6Sj                  NOTE_D6_SHARP
+#define E6j                   NOTE_E6
+#define F6j                   NOTE_F6
+#define F6Sj                  NOTE_F6_SHARP
+#define G6j                   NOTE_G6
+#define G6Sj                  NOTE_G6_SHARP
+#define A6j                   NOTE_A6
+#define A6Sj                  NOTE_A6_SHARP
+#define B6j                   NOTE_B6
+#define NOj                   NONE
